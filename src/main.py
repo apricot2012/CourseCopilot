92a791771a0bdb7dffab13449aa7d2bd22341a16
Jack Li <60091201+JackLiJXL@users.noreply.github.com>
2023-04-27 12:54:12 -0700

    econ qs

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from src.ingestor import Ingestor
 from src.models import tf_idfExtractor, dpr_Extractor, hyde_Extractor
 from os import listdir
@@ -43,4 +44,51 @@ for t in text_directories:
             docs_as_string = [doc.page_content for doc in docs]
             print(docs_as_string)
             print(eval.percentage_contained(groundtruth, docs_as_string))
+=======
+from src.ingestor import Ingestor
+from src.models import tf_idfExtractor, dpr_Extractor, hyde_Extractor
+from os import listdir
+from os.path import isfile, join, isdir
+from pathlib import Path
+import src.evaluator as eval
+from src.enquirer import Enquirer
+
+ingestor = Ingestor()
+# ingestor.ingest()
+
+segment_length = 512
+top_k = 5
+
+tf = tf_idfExtractor(segment_length, top_k)
+# dpr = dpr_Extractor(segment_length, top_k)
+# hyde = hyde_Extractor(segment_length, top_k)
+# models = [tf, dpr, hyde]
+models = [tf]
+
+question_path = './data/questions'
+extracted_path = './data/extracted'
+groundtruth_path = './data/groundtruth'
+text_path = './data/tokenized'
+
+text_directories = [t for t in listdir(text_path) if isdir(join(text_path, t))]
+for t in text_directories:
+    text_file_paths = [join(text_path, t, f) for f in listdir(join(text_path, t))]
+    question_files = sorted([join(question_path, t, f) for f in listdir(join(question_path, t)) if isfile(join(question_path, t, f))])
+    question_names = [Path(q).stem for q in question_files]
+
+    enquirer = Enquirer()
+
+    for i in range(len(question_files)):
+        with open(question_files[i]) as f:
+            query = f.readline()
+        with open(join(groundtruth_path, t, question_names[i] + '.txt')) as f:
+            lines = f.readlines()
+            groundtruth = eval.lines_to_relevant_strings(lines, text_file_paths, segment_length)
+
+        for model in models:
+            docs = model.infer_relevant_docs(text_file_paths, query)
+            docs_as_string = [doc.page_content for doc in docs]
+            print(docs_as_string)
+            print(eval.percentage_contained(groundtruth, docs_as_string))
+>>>>>>> Stashed changes
             print(enquirer.perform_qa(docs, query))
